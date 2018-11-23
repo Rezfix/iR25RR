c1eb24e0c2fc70715f01580f408a8a8f9b80cea4
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-23 19:07:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ client.on('ready', () => {
       console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
       console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
       console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
 
 //AutoRole
 
