4edbc428844e7d78ba43e2164ae914295fcde4cc
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-12-27 10:11:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 client.on('ready', () => {
     client.user.setGame('www.NextaHost.com','https://www.twitch.tv/RezfixServer');
-      console.log('NextaCommunity');
+      console.log('NextaHost');
       console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
       console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
       console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
