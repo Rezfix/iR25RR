67bc194529161dc3195ff21fd352fdb06373a3c6
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-12-27 10:15:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -635,18 +635,6 @@ client.on("message", async message => {
   }
   });
 
-client.on('message',async msg => {//Toxic Codes
-  var p = "-";//Toxic Codes
-  if(msg.content.startsWith(p + "setuser")) {//Toxic Codes
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**');//Toxic Codes
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');//Toxic Codes
-  msg.guild.createChannel('Members → ${client.users.size}` , 'voice').then(time => {//Toxic Codes
-    });//Toxic Codes
-
-  }
- 
-});
-
 client.on("message", msg => { //Toxic Codes CopyRight & Me 92.♥#0092
   if(msg.author.bot) return;
   if(msg.channel.type === 'dm') return;
