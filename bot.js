06b4dc3bfddf5f72a23f8fb6f8ee541da23c18be
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-01-29 13:52:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
+const config = require('../config.json');
 var prefix = "!";
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
@@ -13,7 +14,7 @@ console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~Nexta Network~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  Nexta Network " ] servers! [ " ${client.guilds.size} " ] Users! [ " ${client.users.size} " ]`); 
+console.log(`Logged in as  * [ "  Nexta Network! " ] servers! [ " ${client.guilds.size} " ] Users! [ " ${client.users.size} " ]`); 
 
 });
 
@@ -1641,5 +1642,41 @@ client.on("guildMemberAdd", (member) => {
             message.channel.send("تم تغيير الاسم الى: " + changenick + "")
         }
     }});
+
+const log = (msg) => { console.log(msg) }
+
+module.exports = {
+    help: {
+        name:         "broadcast",
+        aliases:      ['bc']
+    },
+    run: async (bot, msg, args) => {
+        if(!msg.member.hasPermission('ADMINISTRATOR')) return bot.error(msg.channel, "Whoops!", "You don't meet the requirements to use this command!")
+        let message     = args.join(' ');
+        if(!message) return bot.error(msg.channel, "Invaild arguments!", `Please type the message you want to broadcast after the command.`);
+        if(message.length > 2000) return bot.error(msg.channel, "Whoops!", 'Message length cannot be longer than 2000 characters.');
+        
+        let a = 0;
+        let b = 0;
+        let c = 0;
+        let m = `Successfully sent to ${a}, failed to sent to ${b}, totally ${c}`;
+        msg.channel.send(m).then(m2 => {
+            msg.guild.members.forEach(member => {
+                member.send(message).then(x => {
+                    c++;
+                    a++;
+                    if(c == msg.guild.memberCount || c == msg.guild.members.size) m2.edit(`**Done**\n**Successfully sent to ${a}, failed to sent to ${b}, totally ${c}**`);
+                    else m2.edit(`**Successfully sent to ${a}, failed to sent to ${b}, totally ${c}**`);
+                }).catch(er => {
+                    c++;
+                    b++;
+                    if(c == msg.guild.memberCount || c == msg.guild.members.size) m2.edit(`**Done**\n**Successfully sent to ${a}, failed to sent to ${b}, totally ${c}**`);
+                    else m2.edit(`**Successfully sent to ${a}, failed to sent to ${b}, totally ${c}**`);
+                });
+                
+            });
+        });
+    }
+}
 
 client.login(process.env.BOT_TOKEN);
